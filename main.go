b20bc4d022da6d235360efa277ae8ecb40e0afab
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2024-12-05 00:08:00 -0500

    Chi router included

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port= "8000"
+	}
+
+	router := chi.NewRouter()
+
+	//
+
+	router.Get("/api-1", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusAccepted)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"success": "Access granted for api 1",
+		})
+	})
+
+
+	router.Get("/api-2", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusAccepted)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"success": "Access granted for api 2",
+		})
+	})
+
+	srv := &http.Server{
+		Handler: router,
+		Addr : ":" + port,
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil{
+		log.Fatal(err)
+	}
 }
