9fe1b2c4d6d7ddf1b2c673dbd45d03909a02e0b8
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2024-12-05 16:35:54 -0500

    Login implemented

--- controllers/userController.go
+++ controllers/userController.go
@@ -13,7 +13,14 @@ import (
 	helper "github.com/diegobermudez03/golang-jwt-auth/helpers"
 	"github.com/diegobermudez03/golang-jwt-auth/models"
 	"github.com/go-chi/chi/v5"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func passwordToHash(password string) string{
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed)
+
+}
 
 func Signup(w http.ResponseWriter, r *http.Request){
 	var user models.User = models.User{}
@@ -51,7 +58,7 @@ func Signup(w http.ResponseWriter, r *http.Request){
 	}
 
 	_, err = database.Db.ExecContext(ctx, "INSERT INTO users(first_name, last_name, password, email, phone, token, user_type, refresh_token) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
-		user.FirstName, user.LastName, user.Password, user.Email, user.Phone, token, user.UserType, refreshToken,
+		user.FirstName, user.LastName, passwordToHash(user.Password), user.Email, user.Phone, token, user.UserType, refreshToken,
 	)
 
 	if err != nil{
@@ -67,6 +74,50 @@ func Signup(w http.ResponseWriter, r *http.Request){
 }
 
 func Login(w http.ResponseWriter, r *http.Request){
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" || user.Password == ""{
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "No given email or password"})
+		return 
+	}
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
+	defer cancel()
+
+	var storedUser models.User
+	err := database.Db.QueryRowContext(ctx, "SELECT * FROM users WHERE email = $1 LIMIT 1", user.Email).Scan(&storedUser.ID, &storedUser.FirstName, &storedUser.LastName, &storedUser.Password, &storedUser.Email, 
+		&storedUser.Phone, &storedUser.Token, &storedUser.UserType, &storedUser.RefreshToken, &storedUser.CreatedAt, &storedUser.UpdatedAt)
+	if err != nil{
+		if err == sql.ErrNoRows{
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Email doesn't exist"})
+			return 
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return 
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil{
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Incorrect password"})
+		return 
+	}
+	token, refreshToken, _ := helper.GenerateAllTokens(storedUser.Email, storedUser.ID, storedUser.FirstName, storedUser.LastName, storedUser.UserType)
+	
+	_, err = database.Db.ExecContext(ctx, "UPDATE users SET token = $1, refresh_token = $2 WHERE email = $3", token, refreshToken, storedUser.Email)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return 
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"token" : token,
+		"refreshRoken" : refreshToken,
+	})
 
 }
 
