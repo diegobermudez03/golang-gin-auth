efd6d87d228b7136c372ea767ac6d90b79114434
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2024-12-04 23:49:30 -0500

    Model created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diegobermudez03/golang-jwt-auth
+
+go 1.22.6
---
+++ models/userModel.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           string 	`json:"id"`
+	FirstName    string 	`json:"first_name" validate:"required, min=2, max=100"`
+	LastName     string 	`json:"last_name" validate:"required, min=2, max=100"`
+	Password     string 	`json:"password" validate:"required, min=6, max=12"`
+	Email        string 	`json:"email" validate:"email, required"`
+	Phone        string		`json:"phone" validate:"required"`
+	Token        string		`json:"token"`
+	UserType     string		`json:"user_type" validate:"required"`
+	RefreshToken string		`json:"refresh_token"`
+	CreatedAt    time.Time	`json:"created_at"`
+	UpdatedAt	time.Time	`json:"updated_at"`
+	UserId		string		`json:"user_id"`
+}
